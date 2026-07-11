9df64113385911960d4840ae005d6f8260196422
Yashraj Suman <yashraj.55.mhk@gmail.com>
2026-07-11 07:27:28 +0530

    Add Playwright login automation

--- playwright-automation/src/test/java/com/yashraj/playwright/FirstDemo.java
+++ playwright-automation/src/test/java/com/yashraj/playwright/FirstDemo.java
@@ -1,5 +1,7 @@
 package com.yashraj.playwright;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.nio.file.Paths;
 
 import org.testng.Assert;
@@ -12,6 +14,7 @@ import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 
 public class FirstDemo {
 
@@ -25,21 +28,45 @@ public class FirstDemo {
 				.setExecutablePath(Paths.get("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")));
 		BrowserContext context = browser.newContext();
 		page = context.newPage();
-		page.setViewportSize(1920, 1080);
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int screenWidth = (int) screenSize.getWidth();
+		int screenHeight = (int) screenSize.getHeight();
+		// Set the viewport size of the page to the screen size
+		page.setViewportSize(screenWidth, screenHeight);
+//		page.navigate("http://10.82.180.36/Common/Login.aspx");
+
+		
+//		page.setViewportSize(1920, 1080);
 		page.navigate("https://www.saucedemo.com");
 	}
 
 	@Test
-	public void f() {
-		page.locator("#user-name").fill("standard_user");
+	public void run() {
+		/*
+		<input class="input_error form_input" 
+		placeholder="Username" 
+		type="text" data-test="username" 
+		id="user-name" 
+		name="user-name" 
+		autocorrect="off" 
+		autocapitalize="none" 
+		value="" fdprocessedid="cxil0b">
+		*/
+//		page.locator("#user-name").fill("standard_user");		// By ID
+		page.locator("input[name='user-name']").fill("standard_user");		// By CSS selector		
+//		page.locator("//*[@id=\"user-name\"]").fill("standard_user");		// By XPath	--->  Use only when necessary
+
 	    page.locator("#password").fill("secret_sauce");
 	    page.waitForTimeout(1000);
-	    page.locator("#login-button").click();
+//	    page.locator("#login-button").click();
+	    page.getByText("Login").click();
 
 	    String title = page.title();
 	    System.out.println("Page Title: " + title);
 
 	    Assert.assertTrue(page.url().contains("inventory"));
+	    PlaywrightAssertions.assertThat(page).hasURL("https://www.saucedemo.com/inventory.html");
 	}
 	
 	@AfterMethod
